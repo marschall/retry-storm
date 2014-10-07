8fb5ac44f51e87118a0ead144cdd07ae4ab1aec7
Philippe Marschall <philippe.marschall@gmail.com>
2014-10-07 21:56:03 +0200

    bump

---
+++ retry-ejb/src/main/java/com/github/marschall/retrystorm/ejb/ExceptionEjb.java
@@ -0,0 +1,8 @@
+package com.github.marschall.retrystorm.ejb;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class ExceptionEjb {
+
+}
---
+++ retry-ejb/src/main/java/com/github/marschall/retrystorm/ejb/RetryEjb.java
@@ -0,0 +1,49 @@
+package com.github.marschall.retrystorm.ejb;
+
+import static javax.ejb.ConcurrencyManagementType.BEAN;
+
+import java.lang.invoke.MethodHandles;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.ConcurrencyManagement;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.Timeout;
+import javax.ejb.TimerConfig;
+import javax.ejb.TimerService;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+@Singleton
+@Startup
+@ConcurrencyManagement(BEAN)
+public class RetryEjb {
+  
+  private static final Logger LOG = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+  
+  @Resource
+  private TimerService timerService;
+  private TimerConfig timerConfig;
+  
+  
+  @PostConstruct
+  public void initialize() {
+    this.timerConfig = new TimerConfig("RetryEjb", false);
+    this.scheduleTimer();
+  }
+  
+  private void scheduleTimer() {
+    this.timerService.createSingleActionTimer(SECONDS.toMillis(10L), timerConfig);
+  }
+  
+  @Timeout
+  public void onTimeout() {
+    LOG.info("onTimeout");
+    this.scheduleTimer();
+    throw new RuntimeException("retry");
+  }
+
+}
